e5210a4b95021d37ac7f73bcd1baf254425c1627
thanseeh <thanseehabdulla@gmail.com>
2019-12-28 16:12:39 +0530

    local env setup done

--- configurations/db.js
+++ configurations/db.js
@@ -1,9 +1,11 @@
 var mysql = require('mysql')
+const CONFIG = require('./config');
+
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'namoideen',
-  database: 'taxdb'
+  host: CONFIG.db_host,
+  user: CONFIG.db_user,
+  password: CONFIG.db_password,
+  database: CONFIG.db_name
 })
 
 module.exports = connection;
