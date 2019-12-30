7cd878816be2e2d8e997fbb6cd9be1bb6a0ca8c6
thanseeh <thanseehabdulla@gmail.com>
2019-12-30 19:37:31 +0530

    fixes

---
+++ modal/tax.js
@@ -0,0 +1,41 @@
+const Sequelize = require("sequelize");
+var sequelize = require("../configurations/db");
+
+const tax = sequelize.define(
+  "invoice",
+  {
+    id: {
+      field: "tax_id",
+      type: Sequelize.BIGINT(11),
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    taxName: {
+      field: "tax_name",
+      type: Sequelize.STRING
+    },
+    taxPerc: {
+      field: "tax_perc",
+      type: Sequelize.DECIMAL(10, 5)
+    },
+    taxIsactive: {
+      field: "tax_isactive",
+      type: Sequelize.STRING
+    },
+    taxCreated: {
+      field: "tax_created",
+      type: Sequelize.DATE
+    },
+    taxUpdated: {
+      field: "tax_updated",
+      type: Sequelize.DATE
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: false
+  }
+);
+
+module.exports = User;
